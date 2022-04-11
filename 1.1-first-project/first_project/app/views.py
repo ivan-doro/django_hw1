9f35bfbbef70cb2fad9296ddb0ae5f2570d14bdb
ivan-doro <ivandorofeev@icloud.com>
2022-04-11 20:05:48 +0300

    Implemented time_view and workdir_view in views.py. Paths in  urls.py are updated

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,5 +1,8 @@
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
 
 
 def home_view(request):
@@ -8,8 +11,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
     }
     
     # context и параметры render менять не нужно
@@ -23,8 +26,9 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
+    current_time = datetime.now().strftime("%H:%M:%S")
     msg = f'Текущее время: {current_time}'
+    print('' + str(os.listdir()))
     return HttpResponse(msg)
 
 
@@ -32,4 +36,5 @@ def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    dir_list = os.listdir()
+    return HttpResponse('' + str(dir_list))
